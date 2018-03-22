6a9e67e556ebd34a3e7c6e3a96d063a1c0cc9d47
Camilla Montonen <camilla@synbyote.io>
2018-03-22 15:27:13 +0000

    [RDSS-1784] Fixing issue with cron filename

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@
 
 cat <<EOF > /etc/cron.d/run-pure-adaptor-cron
 */${PURE_POLL_FREQUENCY} * * * * root /usr/local/bin/python3 \
-app/pure_adaptor/pure_adaptor.py > /proc/1/fd/1 2>/proc/1/fd/2
+/app/pure_adaptor/pure_adaptor.py > /proc/1/fd/1 2>/proc/1/fd/2
 EOF
 
 chmod 0644 /etc/cron.d/run-pure-adaptor-cron
