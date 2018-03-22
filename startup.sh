a3eec24f555c53366977a591e25179e451168ef9
Camilla Montonen <camilla@synbyote.io>
2018-03-22 14:40:42 +0000

    [RDSS-1784] Making PURE_POLL_FREQUENCY an env var

---
+++ startup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Generate cronjob to run Pure adaptor
+
+cat <<EOF > /etc/cron.d/run-pure-adaptor-cron
+*/${PURE_POLL_FREQUENCY} * * * * root /usr/local/bin/python3 \
+app/pure_adaptor/pure_adaptor.py > /proc/1/fd/1 2>/proc/1/fd/2
+EOF
+
+chmod 0644 /etc/cron.d/run-pure-adaptor-cron
+cron -f
