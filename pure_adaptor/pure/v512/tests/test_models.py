5a97c3d5a0ca0270243e02d71b4f6b91e25ce357
Finlay McCourt <finlay.mccourt@digirati.com>
2018-11-09 16:09:52 +0000

    Updating unit tests for 512 pure data.

--- pure_adaptor/pure/v512/tests/test_models.py
+++ pure_adaptor/pure/v512/tests/test_models.py
@@ -61,70 +61,104 @@ class TestPureMessageMappings(object):
         dataset.custom_funcs = remapping_funcs
         return dataset.rdss_canonical_metadata
 
-    def test_object_date(self, canonical_metadata):
-        date_value = '2017-05-16T15:50:27.337+0000'
-        object_date = canonical_metadata['objectDate'][0]
-        assert object_date['dateValue'] == date_value
+    def test_object_date(self, first_dataset, second_dataset):
+        first_value = '2016-01-15T13:00:06.023+0000'
+        first_date = first_dataset['objectDate'][0]
+        second_date = second_dataset['objectDate'][0]
+        second_value = '2016-03-28T09:32:48.911+0000'
+        assert first_date['dateValue'] == first_value
+        assert second_date['dateValue'] == second_value
 
-    def test_object_resourcetype(self, canonical_metadata):
-        res_type = canonical_metadata['objectResourceType']
-        assert res_type == 7
+    def test_object_resourcetype(self, first_dataset, second_dataset):
+        assert first_dataset['objectResourceType'] == 7
+        assert second_dataset['objectResourceType'] == 7
 
-    def test_object_identifier(self, canonical_metadata):
-        obj_id = canonical_metadata['objectIdentifier']
-        url_value = 'https://www.some_institution.ac.uk/ws/'\
-                    'api/59/datasets/2bdd031e-f373-424f-9657-192431ea4a06'
-        doi_value = 'http://dx.doi.org/10.5061/dryad.8638h'
-        assert isinstance(obj_id, list)
-        assert obj_id[0]['identifierValue'] == url_value
-        assert obj_id[1]['identifierValue'] == doi_value
+    def test_object_identifier(self, first_dataset, second_dataset):
+        first_obj_ids = first_dataset['objectIdentifier']
+        second_obj_ids = second_dataset['objectIdentifier']
 
-    def test_person_uuid(self, person_role):
-        sample_uuid = 'ba8c1112-b6de-446b-ac2f-0b95c80a5cc2'
-        assert person_role['person']['personUuid'] == sample_uuid
+        first_url_value = 'https://an_institution.with_pure.ac.uk/ws/'\
+            'api/512/datasets/59e932cf-3a34-4b7b-bf08-f12a3a2c7273'
+        first_doi_value = '10.001100/b5698935-7651-4333-b00e-2ed2e072eb6a'
 
-    def test_person_identifier(self, person_role):
-        p_id = person_role['person']['personIdentifier'][0]
-        assert p_id['personIdentifierValue'] == '250007106'
-        assert p_id['personIdentifierType'] == 1
+        second_url_value = 'https://an_institution.with_pure.ac.uk/ws/'\
+            'api/512/datasets/ac2c0adf-163d-4a27-b975-f2fa9f46d974'
+        assert isinstance(first_obj_ids, list)
+        assert len(first_obj_ids) == 2
+        assert first_obj_ids[0]['identifierValue'] == first_url_value
+        assert first_obj_ids[1]['identifierValue'] == first_doi_value
 
-    def test_person_role(self, person_role):
-        role_mapping = 5
-        assert person_role['role'] == role_mapping
+        assert isinstance(second_obj_ids, list)
+        assert len(second_obj_ids) == 1
+        assert second_obj_ids[0]['identifierValue'] == second_url_value
 
-    def test_person_name(self, person_role):
-        given_name = 'Alejandro '
-        family_name = 'S\u00e1nchez-Amaro'
-        assert person_role['person']['personGivenNames'] == given_name
-        assert person_role['person']['personFamilyNames'] == family_name
+    def test_object_related_identifiers(self, first_dataset, second_dataset):
+        first_obj_rel_ids = first_dataset['objectRelatedIdentifier']
+        second_obj_rel_ids = second_dataset['objectRelatedIdentifier']
+        assert len(first_obj_rel_ids) == 1
+        assert first_obj_rel_ids[0]['identifier']['identifierType'] == 18
+        assert first_obj_rel_ids[0]['identifier']['identifierValue'] == \
+            'https://an_institution.with_pure.ac.uk/ws/' \
+            'api/512/projects/95d44235-ffab-4260-b9bb-65dd5d2d0fa2'
+        assert first_obj_rel_ids[0]['relationType'] == 11
 
-    def test_person_organisation_unit(self, canonical_metadata):
-        person_role = canonical_metadata['object'
-                                         'PersonRole'][2]
-        ou_uuid = 'e1dc1e3f-980e-4a49-8e46-b0ce898eccbb'
-        ou_uname = 'School of Psychology and Neuroscience'
-        person_ou = person_role['person']['personOrganisationUnit']
-        assert person_ou['organisationUnitUuid'] == ou_uuid
-        assert person_ou['organisationUnitName'] == ou_uname
+        assert len(second_obj_rel_ids) == 3
+        assert second_obj_rel_ids[0]['identifier']['identifierType'] == 18
+        assert second_obj_rel_ids[0]['identifier']['identifierValue'] == \
+            'https://an_institution.with_pure.ac.uk/ws/' \
+            'api/512/projects/44ca6661-b4d6-410d-b071-b91c79c352a9'
+        assert second_obj_rel_ids[0]['relationType'] == 11
 
-    def test_obj_organisation_role(self, canonical_metadata):
-        org_role = canonical_metadata['object'
-                                      'OrganisationRole'][0]
-        organisation = org_role['organisation']
-        assert org_role['organisation']['organisationJiscId'] == 799
-        assert organisation['organisationName'] == 'Dryad'
-        assert organisation['organisationType'] == 8
-        assert org_role['role'] == 9
+        assert second_obj_rel_ids[1]['identifier']['identifierType'] == 18
+        assert second_obj_rel_ids[1]['identifier']['identifierValue'] == \
+            'https://an_institution.with_pure.ac.uk/ws/' \
+            'api/512/research-outputs/lk83ed12-191e-425d-a072-c5ee0b7811ed'
+        assert second_obj_rel_ids[1]['relationType'] == 14
 
-    def test_obj_rights(self, canonical_metadata):
-        obj_rights = canonical_metadata['objectRights']
-        licence = obj_rights['licence'][0]
-        access = obj_rights['access'][0]
-        assert licence['licenceName'] == 'CC BY'
-        assert licence['licenceIdentifier'] == '/dk/atira/pure/dataset/'\
-                                               'documentlicenses/cc-by'
-        assert access['accessType'] == 1
-        assert access['accessStatement'] == 'not present'
+    def test_object_person_role(self, first_dataset, second_dataset):
+        first_person_role = first_dataset['objectPersonRole'][0]
+        second_person_roles_1 = second_dataset['objectPersonRole'][0]
+        second_person_roles_2 = second_dataset['objectPersonRole'][2]
+        assert first_person_role['role'] == 5
+        assert first_person_role['person']['personGivenNames'] == 'Solange'
+        assert first_person_role['person']['personFamilyNames'] == 'Knowles'
+
+        assert second_person_roles_1['role'] == 5
+        assert second_person_roles_1['person']['personGivenNames'] == 'Mark E.'
+        assert second_person_roles_1['person']['personFamilyNames'] == 'Smith'
+
+        assert second_person_roles_2['role'] == 5
+        assert second_person_roles_2['person']['personGivenNames'] == 'Marc'
+        assert second_person_roles_2['person']['personFamilyNames'] == 'Riley'
+
+    def test_person_organisation_unit(self, second_dataset):
+        person = second_dataset['objectPersonRole'][0]['person']
+
+        assert person['personOrganisationUnit']['organisationUnitUuid']\
+            == '42aabe6a-5b0e-4776-a2bd-90c0849be6e7'
+        assert person['personOrganisationUnit']['organisationUnitName']\
+            == 'The Fall'
+
+    def test_obj_organisation_role(self, first_dataset):
+        first_org_role = first_dataset['objectOrganisationRole'][0]
+        assert first_org_role['organisation']['organisationJiscId'] == 99999
+        assert first_org_role['organisation']['organisationName'] == 'Terrible Records'
+        assert first_org_role['organisation']['organisationType'] == 9
+        assert first_org_role['role'] == 5
+
+    def test_obj_rights(self, first_dataset, second_dataset):
+        first_obj_rights = first_dataset['objectRights']
+        second_obj_rights = second_dataset['objectRights']
+        assert first_obj_rights['licence'][0]['licenceName'] == 'not present'
+        assert first_obj_rights['licence'][0]['licenceIdentifier'] == 'not present'
+        assert first_obj_rights['access'][0]['accessType'] == 1
+        assert first_obj_rights['access'][0]['accessStatement'] == 'not present'
+
+        assert second_obj_rights['licence'][0]['licenceName'] == 'CC BY'
+        assert second_obj_rights['licence'][0]['licenceIdentifier']\
+            == '/dk/atira/pure/dataset/documentlicenses/cc-by'
+        assert second_obj_rights['access'][0]['accessType'] == 1
+        assert second_obj_rights['access'][0]['accessStatement'] == 'not present'
 
 
 class TestPureDataset(object):
